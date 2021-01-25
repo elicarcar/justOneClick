f7aacc005ac98ebc14575221ec8eadc8963ff3e5
elicarcar <elisabetta.carcar@gmail.com>
2021-01-25 12:17:16 +0100

    work on about page

---
+++ frontend/src/components/Intro/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="intro">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Intro',
+  props: { title: String, description: String },
+}
+</script>
